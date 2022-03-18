bf29d5491310b5f77ba82635568ff6901e53141b
Shayan Dasgupta <shayandasgupta1502@gmail.com>
2022-03-19 02:26:07 +0530

    added new methods to Board class

--- board.py
+++ board.py
@@ -11,59 +11,58 @@ class Board:
         self.state = {
             "available_moves": [0, 1, 2, 3, 4, 5, 6, 7, 8],
             "X_moves": [],
-            "O_moves": [],
-            "all_moves": []
+            "O_moves": []
         }
-        # visual representation of the board via a matrix
-        self.board = [[' ', ' ', ' '],
-                      [' ', ' ', ' '],
-                      [' ', ' ', ' ']]
 
-    def moves_available(self):
+    def movesAvailable(self):
         return len(self.state["available_moves"]) != 0
 
-    def legal_move(self, move):
-        if self.moves_available():
+    def legalMove(self, move):
+        if self.movesAvailable():
             return move in self.state["available_moves"]
         return False
 
-    # updating the board along with the state
-    def update_board(self, move, shape):
-        self.update_state(move, shape)
-
+    # updating the state of the board
+    def update(self, move, shape):
         state = self.state
-        for pos in range(9):
-            if pos in state["all_moves"]:
-                if pos in state["X_moves"]:
-                    self.board[math.floor(pos / 3)][pos % 3] = 'X'
-                elif pos in state["O_moves"]:
-                    self.board[math.floor(pos / 3)][pos % 3] = 'O'
-
-    # updating the state alone; may join it with method update_board()
-    def update_state(self, move, shape):
-        state = self.state
-
+        # updating state
         if shape == 'X':
             state["X_moves"].append(move)
         else:
             state["O_moves"].append(move)
+        state["available_moves"].remove(move)
 
-        state["all_moves"] = state["X_moves"] + state["O_moves"]
-        for move in state["all_moves"]:
-            if move in state["available_moves"]:
-                state["available_moves"].remove(move)
+    def isWinner(self, shape):
+        if shape == 'X':
+            moves = self.state["X_moves"]
+        else:
+            moves = self.state["O_moves"]
 
-    def print_board(self):
-        for row in self.board:
-            for pos in row:
-                print(f"|{pos}|", end="")
-            print()
+        # checking horizontal win
+        if (0 in moves and 1 in moves and 2 in moves) or \
+                (3 in moves and 4 in moves and 5 in moves) or \
+                (6 in moves and 7 in moves and 8 in moves):
+            return True
 
-    # printing an initial layout of the board with boxes numbered
-    def print_initial_board(self):
-        counter = 0
-        for i in range(3):
-            for j in range(3):
-                print(f"|{counter}|", end="")
-                counter += 1
-            print()
+        # checking vertical win
+        if (0 in moves and 3 in moves and 6 in moves) or \
+                (1 in moves and 4 in moves and 7 in moves) or \
+                (2 in moves and 5 in moves and 8 in moves):
+            return True
+
+        # checking diagonal win
+        if (0 in moves and 4 in moves and 8 in moves) or \
+                (2 in moves and 4 in moves and 6 in moves):
+            return True
+
+        return False
+
+    # undoing a move
+    def undo(self, move, shape):
+        state = self.state
+        # undoing state
+        if shape == 'X':
+            state["X_moves"].remove(move)
+        else:
+            state["O_moves"].remove(move)
+        state["available_moves"].append(move)
